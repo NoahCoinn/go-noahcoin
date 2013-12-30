3ece374e7b0c0978aa027190d242c0fc75392089
obscuren <obscuren@obscura.com>
2013-12-30 01:09:57 +0100

    Split up db and trie and added interface

--- database.go
+++ database.go
@@ -8,12 +8,12 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
-type Database struct {
+type LDBDatabase struct {
 	db   *leveldb.DB
 	trie *Trie
 }
 
-func NewDatabase() (*Database, error) {
+func NewLDBDatabase() (*LDBDatabase, error) {
 	// This will eventually have to be something like a resource folder.
 	// it works on my system for now. Probably won't work on Windows
 	usr, _ := user.Current()
@@ -25,7 +25,7 @@ func NewDatabase() (*Database, error) {
 		return nil, err
 	}
 
-	database := &Database{db: db}
+	database := &LDBDatabase{db: db}
 
 	// Bootstrap database. Sets a few defaults; such as the last block
 	database.Bootstrap()
@@ -33,63 +33,24 @@ func NewDatabase() (*Database, error) {
 	return database, nil
 }
 
-func (db *Database) Bootstrap() error {
+func (db *LDBDatabase) Bootstrap() error {
 	db.trie = NewTrie(db)
 
 	return nil
 }
 
-func (db *Database) Put(key []byte, value []byte) {
+func (db *LDBDatabase) Put(key []byte, value []byte) {
 	err := db.db.Put(key, value, nil)
 	if err != nil {
 		fmt.Println("Error put", err)
 	}
 }
 
-func (db *Database) Close() {
-	// Close the leveldb database
-	db.db.Close()
-}
-
-type Trie struct {
-	root string
-	db   *Database
-}
-
-func NewTrie(db *Database) *Trie {
-	return &Trie{db: db, root: ""}
-}
-
-func (t *Trie) Update(key string, value string) {
-	k := CompactHexDecode(key)
-
-	t.root = t.UpdateState(t.root, k, value)
-}
-
-func (t *Trie) Get(key []byte) ([]byte, error) {
+func (db *LDBDatabase) Get(key []byte) ([]byte, error) {
 	return nil, nil
 }
 
-// Inserts a new sate or delete a state based on the value
-func (t *Trie) UpdateState(node, key, value string) string {
-	if value != "" {
-		return t.InsertState(node, key, value)
-	} else {
-		// delete it
-	}
-
-	return ""
-}
-
-func (t *Trie) InsertState(node, key, value string) string {
-	return ""
-}
-
-func (t *Trie) Put(node []byte) []byte {
-	enc := Encode(node)
-	sha := Sha256Bin(enc)
-
-	t.db.Put([]byte(sha), enc)
-
-	return sha
+func (db *LDBDatabase) Close() {
+	// Close the leveldb database
+	db.db.Close()
 }
--- database_test.go
+++ database_test.go
@@ -2,41 +2,5 @@ package main
 
 import (
 	_ "fmt"
-	"testing"
+	_ "testing"
 )
-
-func TestTriePut(t *testing.T) {
-	db, err := NewDatabase()
-	defer db.Close()
-
-	if err != nil {
-		t.Error("Error starting db")
-	}
-
-	key := db.trie.Put([]byte("testing node"))
-
-	data, err := db.db.Get(key, nil)
-	if err != nil {
-		t.Error("Nothing at node")
-	}
-
-	s, _ := Decode(data, 0)
-	if str, ok := s.([]byte); ok {
-		if string(str) != "testing node" {
-			t.Error("Wrong value node", str)
-		}
-	} else {
-		t.Error("Invalid return type")
-	}
-}
-
-func TestTrieUpdate(t *testing.T) {
-	db, err := NewDatabase()
-	defer db.Close()
-
-	if err != nil {
-		t.Error("Error starting db")
-	}
-
-	db.trie.Update("test", "test")
-}
